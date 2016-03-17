e615b3657539829ea337a5950defdeaa40feb216
FinAndJake <alexander.advokat@rambler.ru>
2016-03-17 21:07:07 +0200

    first commit

---
+++ controllers/rules.controller.php
@@ -0,0 +1,14 @@
+<?php
+
+class RulesController extends Controller {
+
+    public function index(){
+
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: Alexander
+ * Date: 17.03.2016
+ * Time: 20:22
+ */ 
